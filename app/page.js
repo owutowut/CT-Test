ee0bd706397e037a5fbd2c78f6e922d4e4f60f38
tam <sarawut_bm@outlook.com>
2024-07-30 12:41:24 +0700

    init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,179 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import sampleData from "@/constant/sampleData";
 
 export default function Home() {
+  const [inputTest1, setInputTest1] = useState({
+    orderDate: "2024-07-29",
+    status: "Processing",
+  });
+  const [outputTest1, setOutputTest1] = useState([]);
+
+  const get_orders_by_date = (orderDate, status) => {
+    if (orderDate) {
+      return sampleData.orders
+        .filter((order) => {
+          const dataOrderDate = new Date(order.orderDate);
+          const year = dataOrderDate.getFullYear();
+          const month = String(dataOrderDate.getMonth() + 1).padStart(2, "0");
+          const day = String(dataOrderDate.getDate()).padStart(2, "0");
+          const newDataOrderDate = `${year}-${month}-${day}`;
+          if (status !== "None" && status !== "") {
+            return (
+              newDataOrderDate === orderDate.trim() &&
+              order.status === status.trim()
+            );
+          } else {
+            return newDataOrderDate === orderDate.trim();
+          }
+        })
+        .map((order) => ({
+          ...order,
+          totalValue: order.items.reduce(
+            (total, item) => total + item.price * item.quantity,
+            0
+          ),
+        }));
+    } else {
+      if (status !== "None" && status !== "") {
+        return sampleData.orders
+          .filter((order) => order.status === status.trim())
+          .map((order) => ({
+            ...order,
+            totalValue: order.items.reduce(
+              (total, item) => total + item.price * item.quantity,
+              0
+            ),
+          }));
+      } else {
+        return sampleData.orders.map((order) => ({
+          ...order,
+          totalValue: order.items.reduce(
+            (total, item) => total + item.price * item.quantity,
+            0
+          ),
+        }));
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (sampleData) {
+      const result = get_orders_by_date(
+        inputTest1.orderDate,
+        inputTest1.status
+      );
+      setOutputTest1(result);
+    }
+  }, [inputTest1]);
+
+  const [inputTest2, setInputTest2] = useState(0);
+  const [outputTest2, setOutputTest2] = useState(0);
+
+  const test2 = "F(0) = 0, F(1) = 1 F(n) = F(n - 1) + F(n - 2), for n > 1.";
+
+  const f = (n) => {
+    if (n <= 1) {
+      return n;
+    } else {
+      return f(n - 1) + f(n - 2);
+    }
+  };
+
+  useEffect(() => {
+    if (0 <= inputTest2 <= 30) {
+      const result = f(inputTest2);
+      setOutputTest2(result);
+    }
+  }, [inputTest2]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="bg-slate-800 flex justify-center items-center h-full min-h-screen w-full p-[4rem]">
+      <div className="h-full w-full space-y-[2rem]">
+        <div className="p-[4rem] space-y-[2rem] bg-black rounded-lg overflow-auto">
+          <h2>Test1</h2>
+          <div className="space-y-[1rem]">
+            <div className="flex space-x-[1rem]">
+              <p>orderDate : </p>
+              <input
+                className="text-black py-1 px-2 rounded-md"
+                type="string"
+                value={inputTest1.orderDate}
+                onChange={(e) => {
+                  setInputTest1((prevState) => ({
+                    ...prevState,
+                    orderDate: e.target.value,
+                  }));
+                }}
+              />
+            </div>
+            <div className="flex space-x-[1rem]">
+              <p>status : </p>
+              <input
+                className="text-black py-1 px-2 rounded-md"
+                type="string"
+                value={inputTest1.status}
+                onChange={(e) => {
+                  setInputTest1((prevState) => ({
+                    ...prevState,
+                    status: e.target.value,
+                  }));
+                }}
+              />
+            </div>
+          </div>
+          <div className="space-y-[1rem]">
+            <p>orderDate : {inputTest1.orderDate}</p>
+            <p>status : {inputTest1.status}</p>
+            {outputTest1.length > 0 && (
+              <table className="text-white border">
+                <thead>
+                  <tr>
+                    <th className="border p-4">Order ID</th>
+                    <th className="border p-4">Customer ID</th>
+                    <th className="border p-4">Order Date</th>
+                    <th className="border p-4">Status</th>
+                    <th className="border p-4">Total Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {outputTest1.map((order) => (
+                    <tr key={order.orderId}>
+                      <td className="border p-4">{order.orderId}</td>
+                      <td className="border p-4">{order.customerId}</td>
+                      <td className="border p-4">{order.orderDate}</td>
+                      <td className="border p-4">{order.status}</td>
+                      <td className="border p-4">
+                        ${order.totalValue.toFixed(2)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+        <div className="p-[4rem] space-y-[2rem] bg-black rounded-lg overflow-auto">
+          <h2>Test2</h2>
+          <p>{test2}</p>
+          <div className="flex space-x-[1rem]">
+            <p>input : </p>
+            <input
+              className="text-black py-1 px-2 rounded-md"
+              type="number"
+              value={inputTest2}
+              onChange={(e) => {
+                setInputTest2(e.target.value);
+              }}
             />
-          </a>
+          </div>
+          <div>
+            <p>input : {inputTest2}</p>
+            <p>output : {outputTest2}</p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
